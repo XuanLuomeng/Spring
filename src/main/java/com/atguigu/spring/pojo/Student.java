c09d4584b762d9ccf7bd3a3e0dd3374102de4220
XuanLuomeng <1079716693@qq.com>
2022-10-23 13:05:46 +0800

    first

--- src/main/java/com/atguigu/spring/pojo/Student.java
+++ src/main/java/com/atguigu/spring/pojo/Student.java
@@ -1,9 +1,31 @@
 package com.atguigu.spring.pojo;
 
-@D
+import lombok.Data;
+
+@Data
 public class Student {
     private Integer sid;
     private String sName;
     private Integer age;
     private String gender;
+
+    public Student() {
+    }
+
+    public Student(Integer sid, String sName, Integer age, String gender) {
+        this.sid = sid;
+        this.sName = sName;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "sid=" + sid +
+                ", sName='" + sName + '\'' +
+                ", age=" + age +
+                ", gender='" + gender + '\'' +
+                '}';
+    }
 }
